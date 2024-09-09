04f6db124b4db3eb6b642b4fbed0498a5b354800
itsmefminsaf <itsmefminsaf@gmail.com>
2024-09-09 17:48:48 +0530

    added images

---
+++ src/componets/CTA.tsx
@@ -0,0 +1,9 @@
+const CTA = () => {
+  return (
+    <button className="max-[450px]:hidden bg-white rounded-2xl text-black p-3 hover:scale-105 hover:font-bold duration-150 text-lg">
+      Book Now !
+    </button>
+  );
+};
+
+export default CTA;
---
+++ src/componets/Hero.tsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <main className="h-96 bg-slate-400">
+      <h1 className="text-[4em] font-bold w-1/3 text-center pt-24">
+        Welcome to <br /> <span className="">Prest</span>
+      </h1>
+    </main>
+  );
+};
+
+export default Hero;
