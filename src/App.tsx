05d24be044ee1dcdb73b5267c1338a5c5dbf5dc8
itsmefminsaf <itsmefminsaf@gmail.com>
2024-09-09 17:48:21 +0530

    added favicon and designed hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Hero from "./componets/Hero";
+import Nav from "./componets/Nav";
+
 const App = () => {
-  return <main>App</main>;
+  return (
+    <>
+      <Nav />
+      <Hero />
+    </>
+  );
 };
 
 export default App;
